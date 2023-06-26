3217ef7e05fea65bef8603f4228e35dec10b4f00
Teamcity <teamcity@dev-autotest-b-16d2.local>
2023-06-26 19:14:43 +0300

    Release 1.0.139

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapsSDK",
-            url: "https://artifactory-external.vkpartner.ru/artifactory/rs-maps-sdk-ios/1.0.137/MapsSDK.xcframework.zip",
-            checksum: "23ddb9fc4378d466e26a26a9f36618fc9efcfb22c25b7203d7d7af645f6da62b"
+            url: "https://artifactory-external.vkpartner.ru/artifactory/rs-maps-sdk-ios/1.0.139/MapsSDK.xcframework.zip",
+            checksum: "220d339a0217c801426767c0ffff749088941ecf5cada3457d782fb501a16883"
         )
     ]
 )
